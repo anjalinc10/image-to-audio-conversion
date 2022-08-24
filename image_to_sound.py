14f6fcedfbdb1daba39de9db96fcb20a87e7fb71
anjali <anjalinathe1010@gmail.com>
2022-08-24 16:02:24 +0530

    comment changes

--- image_to_sound.py
+++ image_to_sound.py
@@ -5,10 +5,8 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
-def image_to_sound(path_to_image):
-    """
-    Function for converting an  image to sound
-    """
+def image_to_sound(path_to_image): # Function for converting an  image to sound
+    
     try:
         loaded_image = Image.open(path_to_image)
         decoded_text = image_to_string(loaded_image)
