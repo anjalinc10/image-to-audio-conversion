155c672cbb7e7d9f6703a32316d82b9b1ee8ff27
anjali <anjalinathe1010@gmail.com>
2022-08-24 17:14:21 +0530

    Changed directory structure, added gitignore

---
+++ main.py
@@ -0,0 +1,5 @@
+from src import image_to_sound as sou
+
+if __name__ == "__main__":
+    sou.image_to_sound("input/image_to_sound.png")
+    # input()
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is Image to text conversion project
